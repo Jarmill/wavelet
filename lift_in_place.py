c84219c687eeed0d0ac3231fe9fae114aa729621
Jared Miller <jared.fr.miller@gmail.com>
2015-12-01 15:03:00 -0500

    CDF part 2
    Got cdf97 wavelet working. there was an error in the calling
    approx_plot, (wavelet = 'icdf97' instead of wavelet = 'cdf97'), which
    resulted in the inverse of db4 rather than cdf97. next objective is
    legall 5/3 wavelet.

--- lift_in_place.py
+++ lift_in_place.py
@@ -309,11 +309,12 @@ def cdf97(S, MAX_LAYER = -1):
         T = p2
 
     #coefficients
-    alpha = -1.586134342
-    beta = -0.05298011854
-    gamma = 0.8829110762
-    delta = 0.4435068522
-    zeta = 1.149604398
+    alpha = -1.5861343420693648
+    beta = -0.0529801185718856
+    gamma = 0.8829110755411875
+    delta = 0.4435068520511142
+    kodd = 1/1.1496043988602418
+    keven = 1.1496043988602418
 
     layers = 0
     step = 2
@@ -328,14 +329,14 @@ def cdf97(S, MAX_LAYER = -1):
         #odd[n] += alpha * (even[n] + even[n+1])
         S[start:-start:step]  += alpha * (S[:-step:step] + S[step::step])
         S[-start] += alpha * (S[-step] + S[0])
-
+        
         #predict 1
         #p1(z) = beta(1 + z)
         #even[n] += beta * (odd[n] + odd[n-1])
 
         S[step::step] += beta * (S[step+start::step] + S[start:-start:step])
         S[0] += beta * (S[start] + S[-start])
-
+        
         #update 2
         #u2(z) = gamma*(1 + z^-1)
         #odd[n] += gamma * (even[n] + even[n+1]
@@ -350,11 +351,11 @@ def cdf97(S, MAX_LAYER = -1):
         S[0] += delta * (S[start] + S[-start])
 
         #normalize
-        #even[n] *= zeta
-        #odd[n]  *= 1/zeta
-        S[::step]  *= zeta
-        S[start::step] *= 1/zeta
-
+        #even[n] *= keven
+        #odd[n]  *= kodd
+        S[::step]  *= keven
+        S[start::step] *= kodd
+        
         step <<= 1
         layers += 1
 
@@ -363,12 +364,13 @@ def cdf97(S, MAX_LAYER = -1):
 def icdf97(W, MAX_LAYER = -1):
     """CDF9/7 inverse transformation, used for JPEG2000"""
     #coefficients
-    alpha = -1.586134342
-    beta = -0.05298011854
-    gamma = 0.8829110762
-    delta = 0.4435068522
-    zeta = 1.149604398
-
+    alpha = -1.5861343420693648
+    beta = -0.0529801185718856
+    gamma = 0.8829110755411875
+    delta = 0.4435068520511142
+    kodd = 1/1.1496043988602418
+    keven = 1.1496043988602418
+    
     layers = 0
     N = len(W)
     step = N
@@ -379,12 +381,12 @@ def icdf97(W, MAX_LAYER = -1):
 
     while step > 1:
         start = step/2
-
+        
         #normalize'
-        #even[n] *= 1/zeta
-        #odd[n]  *= zeta
-        W[::step]  *= 1/zeta
-        W[start::step] *= zeta
+        #even[n] /= keven
+        #odd[n]  /= kodd
+        W[::step]  /= keven
+        W[start::step] /= kodd
 
 
         #predict 2'
@@ -399,19 +401,20 @@ def icdf97(W, MAX_LAYER = -1):
 
         W[start:-start:step] -= gamma * (W[step::step] + W[:-step:step])
         W[-start] -= gamma * (W[-step] +  W[0])
-
+        
         #predict 1'
         #p1(z) = beta(1 + z)
         #even[n] -= beta * (odd[n] + odd[n-1])
 
         W[step::step] -= beta * (W[step+start::step] + W[start:-start:step])
         W[0] -= beta * (W[start] + W[-start])
-
+        
         #update 1
         #u1(z) = alpha(1 + z^-1)
         #odd[n] -= alpha * (even[n] + even[n+1])
-        W[start:-start:step]  -= alpha * (W[:-step:step] + W[step::step])
         W[-start] -= alpha * (W[-step] + W[0])
+        W[start:-start:step]  -= alpha * (W[:-step:step] + W[step::step])
+
 
         step >>= 1
 
@@ -440,7 +443,7 @@ def idwt(W, MAX_LAYER = -1, wavelet = "db4"):
         return idb4(W, MAX_LAYER = MAX_LAYER)
     elif wavelet == "db6":
         return idb6(W, MAX_LAYER = MAX_LAYER)
-    elif wavelet == "icdf97":
+    elif wavelet == "cdf97":
         return icdf97(W, MAX_LAYER = MAX_LAYER)
     else:
         return idb4(W, MAX_LAYER = MAX_LAYER)
@@ -705,7 +708,7 @@ def psnr(truth, estimate):
 
     return snr
 
-def approx_plot(t, W, MAX_LAYER = -1, wavelet = "db4"):
+def approx_plot(t, W, MAX_LAYER = -1, wavelet = "db4", f = []):
     #sequence of approximations plotted, each with 1/2 as much data as the previous
     N = len(W)
     logN = (N).bit_length() - 1
@@ -722,15 +725,8 @@ def approx_plot(t, W, MAX_LAYER = -1, wavelet = "db4"):
         factor = 1
     elif wavelet == "db4":
         factor = 1/np.sqrt(2)
-        #factor = (np.sqrt(3)-1)/np.sqrt(2)
-        #factor = 0.1895161126191866
-        #factor = 1
     elif wavelet == "db6":
-        #factor = 1/np.sqrt(1.9182029462)
         factor = 1/np.sqrt(2)
-        #factor = 1/np.sqrt(4)
-        #factor = 0.22360679774997896*0.958419749158*0.984681346069
-        #factor = 1/1.9182029462
     elif wavelet == "cdf97":
         factor = 1/np.sqrt(2)
 
@@ -740,6 +736,9 @@ def approx_plot(t, W, MAX_LAYER = -1, wavelet = "db4"):
     ax = fig.add_subplot(111)
 
     prev = 1
+
+    if f != []:
+        ax.plot(t, f, label = "truth")
     
     for level in range(MAX_LAYER, 0, -1):
         #use approximation
@@ -748,7 +747,8 @@ def approx_plot(t, W, MAX_LAYER = -1, wavelet = "db4"):
         #print level, np.max(ids), prev/np.max(ids)
         prev = np.max(ids)
         ts = t[step/2::step]
-        name = "truth" if level == MAX_LAYER else "level" + str(level)
+        
+        name = "level" + str(level)
         ax.plot(ts, ids, label = name)
 
         curr_factor *= factor
@@ -814,30 +814,33 @@ def coeff_pyramid(t, W, MAX_LAYER = -1, wavelet = "db4"):
     return fig
 
 if __name__ == "__main__":
-    s = np.array([32.0, 10.0, 20.0, 38.0, 37.0, 28.0, 38.0, 34.0, 18.0, 24.0, 18.0, 9.0, 23.0, 24.0, 28.0, 34.0])
+    #s = np.array([32.0, 10.0, 20.0, 38.0, 37.0, 28.0, 38.0, 34.0, 18.0, 24.0, 18.0, 9.0, 23.0, 24.0, 28.0, 34.0])
     #s = np.arange(8, dtype = np.float64)
     #s = np.array([9.0, 7.0, 3.0, 5.0])
     #s = np.array([1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0])
     #s = np.array([1.0, 3.0, -2.0, 1.5, -0.5, 2.0, 0.0, 1.0])
-    N = 256
+    N = 1024
+    x = np.linspace(0, 2, N)
+    s = doppler(x)
 
-    """
-    #testing cdf97
-    ml = -1
-    print s
-    ds = cdf97(np.copy(s), MAX_LAYER = ml)
-    waveletprint(ds, MAX_LAYER = ml)
-    ids = icdf97(np.copy(ds), MAX_LAYER = ml)
-    print ids
-    """
     
+    #testing cdf97
+    ml = 7
+    wv = "cdf97"
+    #print s
+    ds = dwt(np.copy(s), MAX_LAYER = ml, wavelet = wv)
+    #waveletprint(ds, MAX_LAYER = ml)
+    ids = idwt(np.copy(ds), MAX_LAYER = ml, wavelet = wv)
+    #print ids
+    approx_plot(x, np.copy(ds), MAX_LAYER = ml, wavelet = wv, f = s)
+    
+    """
     x = np.linspace(0, 2, N)
     sigma = 0.30
     noise = np.random.normal(loc = 0.0, scale = sigma, size = N)
-    s = heavisine(x)
+    #s = heavisine(x)
     rs = s + noise
-    ml = -1
-    #wavelet_type = "db4"
+    ml = 1
     wavelet_type = "cdf97"
     #print s
     ds = dwt(np.copy(s), MAX_LAYER = ml, wavelet = wavelet_type)
@@ -849,9 +852,9 @@ if __name__ == "__main__":
 
     #waveletprint(ds, MAX_LAYER = ml)
     #coeff_pyramid(x, ds, MAX_LAYER = ml, wavelet = wavelet_type)
-    approx_plot(x, ds, MAX_LAYER = ml, wavelet = wavelet_type)
-    
-    
+    #approx_plot(x, ds, MAX_LAYER = ml, wavelet = wavelet_type, f = s)
+    """
+
     """
     #coeff_pyramid(x, drs, MAX_LAYER = ml, wavelet = wavelet_type)
 
